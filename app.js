4632d516852866c43d76a394a1e77317604a1bcb
Miguel Mundi <miguelmundiaragones@gmail.com>
2016-07-07 12:54:57 +0200

    port

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use('/public', express.static('public'));
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use('/public', express.static('public'));
-
-app.listen(PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
